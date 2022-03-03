2e602708313005246efaf768af472bec7d62f6b1
Suchismita Tripathy <80840412+sushi15@users.noreply.github.com>
2022-03-03 21:32:05 +0530

    Update tree.h

--- src/tree.h
+++ src/tree.h
@@ -1,12 +1,5 @@
 #include "treeADTDef.h"
 
 node createEl(int lineNo, int parentSymbolID, int symbolID, int isLeafNode); 
-tree_n add_node(node el);
-tree_n add_sibling(tree_n * sib, tree_n * parent, node el);
-// void add_sibling(tree_n * sib,tree_n * parent, tree_n * new_sib);
-tree_n add_child(tree_n * parent, node el );
-// void add_child(tree_n * parent,tree_n * new_child );
-// tree_n* remove_node(tree_n* parent, tree_n* prev_sib, tree_n* node);
-
-
-// tree_node *get_nth_child(tree_node *root, int n);
+treeN createNode(node el);
+void addChild(treeN* parent, treeN* new); 
