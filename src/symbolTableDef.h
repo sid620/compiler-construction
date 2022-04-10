604cd493559a671efddb4932b2b7948b04de1367
Suchismita Tripathy <80840412+sushi15@users.noreply.github.com>
2022-04-10 19:37:57 +0530

    Add files via upload

---
+++ src/symbolTableDef.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <stdio.h> 
+#include <stdbool.h> 
+#include "ast.h"
+
+char* types[] = {"int", "real", "boolean", "rec", "union"}; 
+
+typedef struct entry { 
+    char* varName; 
+    int present; 
+    int lineNo; 
+    int offset; 
+    int type; // Index of array allTypes 
+} entry; 
+
+typedef struct typeInfo { 
+    char* name; 
+    int isParam; 
+    int isPrim; 
+    int typeId; 
+    int fInd; 
+    int width; 
+    entry** fields; 
+    int numFields; 
+    char** aliases; 
+    int numAl; 
+} typeInfo;
+
+typedef struct fInfo { 
+    char* fId; 
+    int numIn; 
+    int numOut; 
+    int inId; 
+    int outId; 
+} fInfo; 
+
+typedef struct table { 
+    entry** entries; // Array of pointers to entries 
+    int numEntries; 
+    fInfo* function; 
+    int currWidth; 
+} table; 
+
+typedef struct symbolTable { 
+    table** tables; // Array of table* : Array of pointers to arrays of entries i.e even single 
+    // entries like 1 variable will have their own entry array with length 1 
+    entry** entries; // Global variables 
+    int numEntries; 
+    int numF; 
+    int numTypes; 
+    int mainOffset; 
+    typeInfo** allTypes; // Pointer to array of typeInfo structs 
+} symbolTable; 
