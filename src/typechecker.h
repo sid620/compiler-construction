98d8eafc57323652d29dddd7adf3cb293f2c1390
Siddharth <f20190064@pilani.bits-pilani.ac.in>
2022-04-14 18:16:36 +0530

    added typechecker.h

---
+++ src/typechecker.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h> 
+#include <stdbool.h> 
+#include "symbolTable.h" 
+#include "lexer.h"
+#include "ast.h"
+#include "parser.h"
+bool isLogOp(astNode *root, grammar G); 
+bool isRelOp(astNode *root, grammar G); 
+int findIDtype(astNode* root, grammar G, symbolTable* sTable, int index); 
+int findNestedType(astNode *root, grammar G, symbolTable *sTable, int index); 
+int matchReturnParams(astNode *temp, grammar G, symbolTable *sTable, int index); 
+int fIndex(char* fName, symbolTable* sTable); 
+int typeCheck(astNode* root, grammar G, symbolTable* sTable, int index); 
